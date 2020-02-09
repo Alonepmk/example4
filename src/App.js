ad2df1801778ef2280e8f86585ce88f219469fdd
Alonepmk <alone21896@gmail.com>
2020-02-09 16:31:10 +1100

    first app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/page1" component={Page1}/>
+      <Route path="/page2" component={Page2}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+class Home extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      password : "",
+    };
+  }
+  doCheck = ()=> {
+    if(this.state.password === "12345"){
+      window.location.assign("/page2");
+    }
+  }
+  getText = (event)=> {
+    this.setState({password: event.target.value});
+  }
+  render(){
+    return(
+      <div>
+        <h1>Home Page</h1>
+        <input type="text" onChange={this.getText}></input>
+        <input type="button" onClick={this.doCheck}></input>
+      </div>
+    );
+  }
+}
+  
+
 
 export default App;
---
+++ src/Page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Page1() {
+  return (
+    <div>
+      <h1>Page1</h1>
+    </div>
+  );
+}
+
+export default Page1;
